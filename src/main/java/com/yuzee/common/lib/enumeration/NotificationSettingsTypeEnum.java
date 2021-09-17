aaba0a339ce1b220d692afb12add5d3ec18c8f12
Gaurang Patel <gaurang@yuzee.com>
2021-09-17 19:12:12 +0530

    move enum for NotificationSettings

---
+++ src/main/java/com/yuzee/common/lib/enumeration/NotificationSettingsTypeEnum.java
@@ -0,0 +1,11 @@
+package com.yuzee.common.lib.enumeration;
+
+public enum NotificationSettingsTypeEnum {
+	REQUESTS,
+	NEWSFEED_POSTS,
+	REMINDERS,
+	BIRTHDAY,
+	MORE_ACTIVITIES_ABOUT_YOU,
+	RECOMMENDATIONS,
+	EVENTS
+}
