b44527ebd24bdda2d8a61b15b29313fa6ee3c642
Gaurang Patel <gaurang@yuzee.com>
2021-10-21 19:21:16 +0530

    create TagDto for add or retrive tags in post db from interaction-service. create method addHashTag in PostHandler for add tag in post-service db

---
+++ src/main/java/com/yuzee/common/lib/dto/interaction/TagDto.java
@@ -0,0 +1,19 @@
+package com.yuzee.common.lib.dto.interaction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TagDto {
+
+	@JsonProperty("tag_id")
+	private String tagId;
+	
+	@JsonProperty("tag_text")
+	private String tagText;
+}
