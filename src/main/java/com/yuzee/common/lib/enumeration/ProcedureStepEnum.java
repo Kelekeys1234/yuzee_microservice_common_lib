0780e565cd3162f5e7a93fd1e0a367554b7a42d3
Gaurang Patel <gaurang@yuzee.com>
2021-11-25 17:56:01 +0530

    change common-lib version 0.0.3 and add isAllowToDelete boolean in ProcedureStepEnum.java

--- src/main/java/com/yuzee/common/lib/enumeration/ProcedureStepEnum.java
+++ src/main/java/com/yuzee/common/lib/enumeration/ProcedureStepEnum.java
@@ -5,18 +5,20 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum ProcedureStepEnum {
-	YUZEE_REVIEW("Yuzee Review"), //TaskNameEnum.YUZEE_REVIEW_STUDENT_APPLICATION
-	APPLICANT_REVIEWED_BY_INSTITUTION("Applicant reviewed by institution"), //TaskNameEnum.INSTITUTE_REVIEW_STUDENT_APPLICATION
-	OFFERS_RECEIVED_BY_APPLICANT("Offers received by applicant"), //TaskNameEnum.MAKE_AN_OFFER
-	LETTER_OF_OFFER_APPROVED_BY_APPLICANT("Letter of offer approved by applicant"), //TaskNameEnum.WAIT_FOR_STUDENT_ACCEPT_OFFER_LETTER
-	COMPLETED_ENVIRONMENT("Completed environment"), //TaskNameEnum.COMPLETED_ENVIRONMENT
-	INFO_SESSION("Info session"), //TaskNameEnum.INFO_SESSION
-	PAYMENT("Payment"), //TaskNameEnum.PAYMENT
-	PRE_TRAINING_REVIEW("Pre training review"), //TaskNameEnum.SCHEDULE_PRE_TRAINING_REVIEW
-	OTHER("Other"), //TaskNameEnum.OTHER`
-	JOB_APPLICATION("Job application");
+	YUZEE_REVIEW("Yuzee Review",false), //TaskNameEnum.YUZEE_REVIEW_STUDENT_APPLICATION
+	APPLICANT_REVIEWED_BY_INSTITUTION("Applicant reviewed by institution",false), //TaskNameEnum.INSTITUTE_REVIEW_STUDENT_APPLICATION
+	OFFERS_RECEIVED_BY_APPLICANT("Offers received by applicant",false), //TaskNameEnum.MAKE_AN_OFFER
+	LETTER_OF_OFFER_APPROVED_BY_APPLICANT("Letter of offer approved by applicant",false), //TaskNameEnum.WAIT_FOR_STUDENT_ACCEPT_OFFER_LETTER
+	COMPLETED_ENVIRONMENT("Completed environment",false), //TaskNameEnum.COMPLETED_ENVIRONMENT
+	INFO_SESSION("Info session",true), //TaskNameEnum.INFO_SESSION
+	PAYMENT("Payment",true), //TaskNameEnum.PAYMENT
+	PRE_TRAINING_REVIEW("Pre training review",true), //TaskNameEnum.SCHEDULE_PRE_TRAINING_REVIEW
+	OTHER("Other",true), //TaskNameEnum.OTHER`
+	JOB_APPLICATION("Job application",true);
 	
 	@Getter private String displayName;
+	
+	@Getter private boolean isAllowToDelete;
 	
 	public String getName() {
 		return this.name();
