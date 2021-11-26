5caadf464fae19237e6e0e741e4e18d4a763ebca
Muhammad Awais Zahid <awais@yuzee.com>
2021-11-26 15:07:47 +0500

    PERSONAL_IDENTITY(true),	AUSTRALIAN_BUSINESS_NUMBER(true);

--- src/main/java/com/yuzee/common/lib/dto/company/CompanyDto.java
+++ src/main/java/com/yuzee/common/lib/dto/company/CompanyDto.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yuzee.common.lib.dto.storage.StorageDto;
 
 import lombok.Data;
 
@@ -94,5 +95,8 @@ public class CompanyDto {
 	
 	@JsonProperty("australian_business_number")
 	private String australianBusinessNumber;
+	
+	@JsonProperty("storages")
+	private List<StorageDto> storages;
 	
 }
