1358dc4106ab806371e7ca52e701ac0d4b57c360
Muhammad Awais Zahid <awais@yuzee.com>
2022-01-14 15:52:57 +0500

    user semester

---
+++ src/main/java/com/yuzee/common/lib/dto/user/SemesterDto.java
@@ -0,0 +1,33 @@
+package com.yuzee.common.lib.dto.user;
+
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yuzee.common.lib.dto.ValidList;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SemesterDto {
+
+	@JsonProperty("id")
+	private String id;
+
+	@NotEmpty(message = "type must not be empty")
+	@JsonProperty("type")
+	private String type;
+
+	@NotEmpty(message = "name must not be empty")
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("description")
+	private String description;
+	
+	@JsonProperty("subjects")
+	ValidList<SemesterSubjectDto> subjects;
+}
---
+++ src/main/java/com/yuzee/common/lib/dto/user/SemesterSubjectDto.java
@@ -0,0 +1,28 @@
+package com.yuzee.common.lib.dto.user;
+
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SemesterSubjectDto {
+
+	@JsonProperty("id")
+	private String id;
+
+	@NotEmpty(message = "name must not be empty")
+	@JsonProperty("name")
+	private String name;
+	
+	@JsonProperty("description")
+	private String description;
+
+	@JsonProperty("grade")
+	private String grade;
+}
