cd65856c98a9ed810328bc8f35603375ea7efb7e
Gaurang Patel <gaurang@yuzee.com>
2022-02-23 11:09:01 +0530

    remove OTHER and ENQUIRY enums from ContactTypeEnum.java

--- src/main/java/com/yuzee/common/lib/enumeration/ContactTypeEnum.java
+++ src/main/java/com/yuzee/common/lib/enumeration/ContactTypeEnum.java
@@ -15,7 +15,6 @@ public enum ContactTypeEnum {
 	PHONE("Phone","phone.svg",false),
 	SKYPE("Skype","skype.svg",false),
 	WEBSITE("Website","web_icon.svg",false),
-	ENQUIRY("Enquiry","enquiry.svg",false),
 	WHATSAPP("WhatsApp","whatsApp.svg",false),
 	GOOGLE_HANGOUT("Google Hangouts","hangout.svg",false),
 	IMO("IMO","imo.svg",false),
@@ -29,8 +28,7 @@ public enum ContactTypeEnum {
 	TIK_TOK("Tik Tok","tikTok.svg",false),
 	VIBER("Viber","viber.svg",false),
 	WE_CHAT("WeChat","weChat.svg",false),
-	YABB_MESSENGER("Yabb Messenger","yab.svg",false),
-	OTHER("Other","other.svg",false);
+	YABB_MESSENGER("Yabb Messenger","yab.svg",false);
 	
 	@Getter private String displayName;
 	@Getter @Setter private String imageName;
