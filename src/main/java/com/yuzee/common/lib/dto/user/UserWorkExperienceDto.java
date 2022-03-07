e7fbb62b9c289cda460047edd8d46b5f0a25b812
Gaurang Patel <gaurang@yuzee.com>
2022-03-07 09:35:59 +0530

    change work experience job description max length

--- src/main/java/com/yuzee/common/lib/dto/user/UserWorkExperienceDto.java
+++ src/main/java/com/yuzee/common/lib/dto/user/UserWorkExperienceDto.java
@@ -57,7 +57,7 @@ public class UserWorkExperienceDto {
 	@JsonProperty("postal_code")
 	private String postalCode;
 	
-	@Size(min = 30, max = 220, message = "{user_work_experience.job_description.size}")
+	@Size(min = 30, max = 3000, message = "{user_work_experience.job_description.size}")
 	@JsonProperty("job_description")
 	private String jobDescription;
 	
