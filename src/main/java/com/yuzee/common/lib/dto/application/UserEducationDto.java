56856dc64d0e229b9ea10ad91401a63ab17bc161
Gaurang Patel <gaurang@yuzee.com>
2022-07-12 13:38:27 +0530

    add attribute of user:-UserApplicationDto to application:-UserApplicationDto

--- src/main/java/com/yuzee/common/lib/dto/application/UserEducationDto.java
+++ src/main/java/com/yuzee/common/lib/dto/application/UserEducationDto.java
@@ -1,5 +1,6 @@
 package com.yuzee.common.lib.dto.application;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,7 +10,9 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yuzee.common.lib.dto.institute.LevelDto;
 import com.yuzee.common.lib.dto.storage.StorageDto;
+import com.yuzee.common.lib.dto.user.SemesterDto;
 
 import lombok.Data;
 
@@ -18,17 +21,9 @@ public class UserEducationDto {
 	
 	@JsonProperty("user_education_id")
 	private String userEducationId;
-	
-	@JsonProperty("source_user_education_id")
-	private String sourceUserEducationId;
-	
-	@NotBlank(message = "application_id is required")
-	@JsonProperty("application_id")
-	private String applicationId;
-	
-	@NotBlank(message = "application_type is required")
-	@JsonProperty("application_type")
-	private String applicationType;
+
+	@JsonProperty("institute_id")
+	private String instituteId;
 	
 	@NotBlank(message = "privacy_level is required")
 	@JsonProperty("privacy_level")
@@ -38,22 +33,10 @@ public class UserEducationDto {
 	@JsonProperty("education_title")
 	private String educationTitle;
 	
-	@JsonProperty("study_mode")
-	private String studyMode;
-
-	@JsonProperty("institute_id")
-	private String instituteId;
-	
 	@NotBlank(message = "institute_name is required")
 	@JsonProperty("institute_name")
 	private String instituteName;
-
-	@JsonProperty("course_name")
-	public String courseName;
 	
-	@JsonProperty("course_language")
-	private String courseLanguage;
-
 	@NotBlank(message = "city is required")
 	@JsonProperty("city")
 	private String city;
@@ -69,10 +52,17 @@ public class UserEducationDto {
 	@NotBlank(message = "country is required")
 	@JsonProperty("country")
 	public String country;
-
-	@JsonProperty("system")
-	public String system;
-
+	
+	@JsonProperty("education_system_id")
+	public String educationSystemId;
+	
+	@JsonProperty("education_system_name")
+	public String educationSystemName;
+	
+	@NotBlank(message = "course_name is required")
+	@JsonProperty("course_name")
+	public String courseName;
+	
 	@JsonProperty("cgpa")
 	public double cgpa;
 
@@ -85,9 +75,53 @@ public class UserEducationDto {
 	@JsonFormat(pattern="yyyy-MM-dd")
 	private Date endDate;
 	
-	@JsonProperty("subjects")
-	private List<UserSubjectDto> subjects = new ArrayList<>();
+	@NotBlank(message = "study_mode is required")
+	@JsonProperty("study_mode")
+	private String studyMode;
+	
+	@JsonProperty("description")
+	private String description;
+	
+	@NotBlank(message = "course_language is required")
+	@JsonProperty("course_language")
+	private String courseLanguage;
+
+	@JsonProperty("institute_logo_url")
+	private String instituteLogoUrl;
+	
+	@NotBlank(message = "level_id is required")
+	@JsonProperty("level_id")
+	private String levelId;
+	
+	@NotNull(message = "latitude is required")
+	@JsonProperty("latitude")
+	private BigDecimal latitude;
+	
+	@NotNull(message = "longitude is required")
+	@JsonProperty("longitude")
+	private BigDecimal longitude;
+	
+	@JsonProperty("result_type")
+	private String resultType;
+	
+	@JsonProperty("level")
+	private LevelDto level;
+
+	@JsonProperty("source_user_education_id")
+	private String sourceUserEducationId;
+	
+	@NotBlank(message = "application_id is required")
+	@JsonProperty("application_id")
+	private String applicationId;
+	
+	@NotBlank(message = "application_type is required")
+	@JsonProperty("application_type")
+	private String applicationType;
+	
+	@JsonProperty("semesters")
+	List<SemesterDto> semesters = new ArrayList<>();
 	
 	@JsonProperty("certificates")
 	List<StorageDto> certificates = new ArrayList<>();	
+
 }
